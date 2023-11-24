8977d4feb476ec6df3e7f3be80c623a22970abfa
kuukua <kuukuasika449@gmail.com>
2023-11-24 17:18:10 +0000

    project1.0

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.addEventListener('DOMContentLoaded', function()) {
+document.addEventListener('DOMContentLoaded', function() {
 const greeting = document.getElementById('greeting');
 if (greeting) {
-  const currentTime = new Date
+const currentTime = new Date().getHours();
+const greetingMessage = currentTime < 12 ? 'Good morning': 'Good afternoon';
+
+greeting.textContent = `${greetingMessage}, welcome to UG Computer Science Department!`;
 }
-}
+});
